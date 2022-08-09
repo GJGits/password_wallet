a5d74104349cb736645c3a3a4fc0a4bf8cecb1c7
GJCode <golla.programmazione@gmail.com>
2022-08-09 12:49:14 +0000

    fix-#25 aggiornati moduli importati da nuovo componente

--- view/src/app/dummies/form-key-value/form-key-value.component.spec.ts
+++ view/src/app/dummies/form-key-value/form-key-value.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ServicesModule } from 'src/app/services/services.module';
+import { DummiesModule } from '../dummies.module';
 
 import { FormKeyValueComponent } from './form-key-value.component';
 
@@ -8,6 +11,7 @@ describe('FormKeyValueComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ServicesModule, RouterTestingModule, DummiesModule],
       declarations: [ FormKeyValueComponent ]
     })
     .compileComponents();
