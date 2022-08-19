7b407e0a7d1996e6072e6f4741de9ded620a3c39
GJCode <golla.programmazione@gmail.com>
2022-08-19 15:22:35 +0000

    fix-#33

--- view/src/rules/subscribe-extends.js
+++ view/src/rules/subscribe-extends.js
@@ -1,21 +1,16 @@
 module.exports = {
+    meta: {
+        messages: {
+            emptyCatch: 'Empty catch block is not allowed.',
+        },
+    },
     create(context) {
         return {
-            ClassDeclaration(node) {
-                let source = context.getSourceCode().getText();
-                let subCount = (source.match(/subscribe/g) || []).length;
-                let subPushCount = (source.match(/subscriptions\$/g) || []).length;
-                let extendsCount = (source.match(/extends WithSubscription/g) || []).length;
-                if (subCount !== subPushCount || (subCount > 0 && extendsCount === 0)) {
-                    context.report({
-                        node: node,
-                        message: "subscribe not correctly implemented, a component that needs \
-                            a subscription must implement the WithSubscription class and push \
-                            the subscription into the subscriptions container. Take a look at \
-                            https://gist.github.com/GJGits/c5503741309850d86ff7a159d57d9a9e ."
-                    });
+            CatchClause(node) {
+                if (node.body.body.length === 0) {
+                    context.report({ node: node.body, messageId: 'emptyCatch' });
                 }
             }
         }
     }
-}
+};
